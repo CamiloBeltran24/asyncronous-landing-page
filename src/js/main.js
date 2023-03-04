fbb09c136d5064a8a84c0c937e9becbce20e5b32
Camilo <cambelf.dev@gmail.com>
2023-03-03 23:11:53 -0500

    update API

--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,36 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC8LeXCWOalN8SxlrPcG-PaQ&part=snippet%2Cid&order=date&maxResults=9'
+const API = 'https://rickandmortyapi.com/api/character'
 
 const content = null || document.getElementById('content')
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '96e182059dmsh660ed9d8a612c35p16f7ebjsnbc2b066cb00b',
-		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-	}
-};
-
 async function fetchData(urlApi) {
-  const response = await fetch(urlApi, options);
+  const response = await fetch(urlApi);
   const data = await response.json();
   return data;
 }
-
-fetch(`${API}`, options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
 
 (async ()=>{
 
   try {
 
-    const videos = await fetchData(API);
+    const characters = await fetchData(API);
+
+    console.log(characters);
     let view = `
-    ${videos.items.map(video =>`
+    ${characters.results.map(character =>`
       <div class="group relative">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          <img src="${character.image}" alt="${character.name}" class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
           <h3 class="text-sm text-gray-700">
             <span aria-hidden="true" class="absolute inset-0"></span>
-            ${video.snippet.title}
+            ${character.name}
           </h3>
         </div>
       </div>
 
-    `).slice(0 , 4).join('')}
+    `).slice(0 , 20).join('')}
     `;
 
     content.innerHTML = view;
